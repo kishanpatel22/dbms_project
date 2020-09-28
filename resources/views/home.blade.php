7372074747fdb5f003194706d72528b6aee788ff
kishan patel <kp768919@gmail.com>
2020-09-29 00:17:54 +0530

    laravel init code structure

---
+++ resources/views/home.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <!-- Add title --> 
+    <title>Home page</title>
+    
+    <!-- meta charset --> 
+    <meta charset="UTF-8">
+    
+</head>
+<body>
+    <!-- HTML body --> 
+    <h1 style="text-align:center"> DBMS project Home page </h1>
+
+</body>
+</html>
